62a896a3b298fc67a7df78cd296a930409d2dc09
evlx87 <evilnyagaming@yandex.ru>
2020-03-03 12:30:23 +0300

    добавлена переменная mid_list для хранения промежуточных значений

--- find_extension.py
+++ find_extension.py
@@ -6,13 +6,19 @@ import re
 def find_ext(input_file):
     """Функция поиска расширений в лог-файле"""
     dir_log = open(input_file, mode='r', encoding='utf8')
-    sort_list = open('output/sort_list_' +
-                     str(time.strftime("%Y_%m_%d_%H_%M_%S")) +
-                     '.txt', 'tw')
+
+    mid_list = []  # Переменная для хранения промежуточных значений
 
     for line in dir_log:
         match = re.findall(r'\.[a-zA-Z]{2,5}', line)
-        sort_list.writelines(set(match))
+        if match not in mid_list:
+            mid_list.append(match)
+
+    sort_list = open('output/sort_list_' +
+                     str(time.strftime("%Y_%m_%d_%H_%M_%S")) +
+                     '.txt', 'tw')
+    for element in mid_list:
+        sort_list.writelines(set(element))
         sort_list.write('\n')
 
     dir_log.close()
